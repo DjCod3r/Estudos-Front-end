34af334ad5de7213ae1188d5503904db6a056b0a
DjCod3r <djonatanschvambach@gmail.com>
2021-06-14 00:25:38 -0300

    Estudos Front-End
    Estudos HTML , CSS

---
+++ ReactJs/app/src/hi.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => [
+        <h1 key='1'>Bom dia {props.nome}</h1>,
+        <h2 key='2' > Nos vemos em breve</h2> ]
+  
+
+
+
+//<React.Fragment>
+//<h1>Bom dia {props.nome}</h1>
+//<h2>Nos vemos em breve</h2> 
+//</React.Fragment>
---
+++ jQuery/js/jQuery/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('div').css('background','red')
+    setTimeout(function(){
+        $('div').css('background','blue')
+    },2000)
+    $('div p').css('color','white')
+} )
+$('body').css('background','black')
